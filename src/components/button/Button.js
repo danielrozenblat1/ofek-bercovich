cc86d516437f83f7b9b389273603b8dacb95e7dc
Your Name <you@example.com>
2024-08-26 12:01:56 +0300

    addedFacebookButtonPixel

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,36 +1,32 @@
 import { useEffect } from "react"
 import styles from "./Button.module.css"
 import ScrollReveal from "scrollreveal"
-const Button=(props)=>{
 
+const Button = (props) => {
+  const handleClick = () => {
+    // Facebook Pixel tracking
+    if (window.fbq) {
+      window.fbq('track', 'Contact');
+    }
 
-    const handleClick = () => {
+    if (props.href) {
+      window.open(props.href);
+      return;
+    }
 
+    const phoneNumber = "+972542002965";
+    const message = props.message || "היי אופק, אשמח לשמוע עוד על..";
+    const encodedMessage = encodeURIComponent(message);
+    const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+    
+    window.open(whatsappURL, "_blank");
+  };
 
-        if(props.href){
-    
-    window.open(props.href);
-    return ;
-    
-        }
-        const phoneNumber = "+972542002965";
-        // Set the message content
-        const message =props.message || "היי אופק, אשמח לשמוע עוד על..";
-    
-        // Encode the message for the URL
-        const encodedMessage = encodeURIComponent(message);
-        // Construct the WhatsApp message URL with phone number and message
-        const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
-      
-        // Open the URL in a new tab
-        window.open(whatsappURL, "_blank");
-    
-      };
-    
-    return <>
+  return (
     <div className={styles.center}>
-    <button onClick={handleClick} className={styles.button}>{props.text}</button>
+      <button onClick={handleClick} className={styles.button}>{props.text}</button>
     </div>
-    </>
+  );
 }
+
 export default Button
