6421cd386c442f3434c20d3f23689a583cc3182a
Your Name <you@example.com>
2024-10-23 22:05:19 +0700

    fixedFORM

--- src/components/form/FormScreen.js
+++ src/components/form/FormScreen.js
@@ -12,58 +12,59 @@ const FormScreen = () => {
 
   const submitHandler = async (e) => {
     e.preventDefault();
+    
     const name = nameRef.current.value;
     const phone = phoneRef.current.value;
     const email = emailRef.current.value;
     const agree = agreeRef.current.checked;
 
     if (name.trim().length <= 2) {
-      alert("אנא הכנס שם מלא");
-      return;
+        alert("אנא הכנס שם מלא");
+        return;
     }
     if (phone.trim().length !== 10) {
-      alert("אנא הכנס מספר טלפון הכולל 10 ספרות");
-      return;
+        alert("אנא הכנס מספר טלפון הכולל 10 ספרות");
+        return;
     }
     if (!email.includes("@")) {
-      alert("אנא הכנס מייל תקין");
-      return;
+        alert("אנא הכנס מייל תקין");
+        return;
     }
     if (!agree) {
-      alert("אנא אשר/י לקבל דואר במייל");
-      return;
+        alert("אנא אשר/י לקבל דואר במייל");
+        return;
     }
 
     const formData = {
-      name: name,
-      phone: phone,
-      email: email,
-      reciver: reciver,
+        name: name,
+        phone: phone,
+        email: email,
+        reciver: reciver,
     };
 
     // שליחת הבקשה הראשונה
     const response = await fetch('https://dynamic-server-dfc88e1f1c54.herokuapp.com/leads/newLead', {
-      method: "POST",
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(formData),
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData),
     });
 
     if (response.ok) {
-      // שליחת הבקשה השנייה
-      try {
+        // שליחת הבקשה השנייה עם mode: 'no-cors'
         const secondApiUrl = `https://panel.sendmsg.co.il/AddUserFromSite.aspx?cellphone=${encodeURIComponent(phone)}&email=${encodeURIComponent(email)}&1=${encodeURIComponent(name)}&form=335852__a46f180f-5b91-4fb5-ac2c-3a17c3917fcb`;
         
-        await fetch(secondApiUrl);
-        
-        // ניקוי הטופס והצגת ההודעה רק אם שתי הבקשות הצליחו
+        await fetch(secondApiUrl, {
+            mode: 'no-cors'  // הוספת mode: 'no-cors' לפתרון בעיית ה-CORS
+        });
+
+        // ניקוי הטופס והצגת ההודעה
         alert("שמרנו את הפרטים שלך, ניצור קשר בימים הקרובים");
         nameRef.current.value = "";
         phoneRef.current.value = "";
         emailRef.current.value = "";
         agreeRef.current.checked = false;
-      } catch (error) {
-        console.error("שגיאה בשליחת הנתונים ל-API השני:", error);
-      }
     }
     
     console.log(formData);
@@ -84,7 +85,7 @@ const FormScreen = () => {
             מאשרת קבלת דיוור במייל
           </label>
           <button className={styles.button} onClick={submitHandler}>
-          שלחי
+      שלחי
           </button>
         </div>
       </form>
